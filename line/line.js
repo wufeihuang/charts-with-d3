51c51a0424b981694f794a55bd2e63a77a1fc21d
wfh <1710333107@qq.com>
2019-04-30 01:05:22 +0800

    feat: 折线图

---
+++ line/line.js
@@ -0,0 +1,49 @@
+function init() {
+  const xScale = d3.scalePoint()
+    .domain(d3.range(data.length))
+    .range([0, mainWidth])
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([mainHeight, 0])
+
+  const line = d3.line()
+    .x((d, i) => xScale(i))
+    .y(d => yScale(d))
+
+  function drawLine() {
+    g
+      .append('path')
+      .attr('d', line(data))
+      .attr('fill', 'none')
+      .attr('stroke', '#000')
+  }
+
+  function drawAxis() {
+    g.append('g')
+      .attr('transform', `translate(0, ${mainHeight})`)
+      .call(
+        d3.axisBottom()
+          .scale(xScale)
+      )
+
+    g.append('g')
+      .call(
+        d3.axisLeft()
+          .scale(yScale)
+      )
+  }
+
+  function render() {
+    updateTitle()
+    updateSvg()
+    drawLine()
+    drawAxis()
+  }
+
+  render()
+
+  return render
+}
+
+const render = init()
